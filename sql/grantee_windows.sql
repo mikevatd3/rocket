7ab87a15ed4f66fd1554c36be37363ba6f310fcf
mikevatd3 <115018438+mikevatd3@users.noreply.github.com>
2026-06-16 15:32:46 -0400

    Add SQL query for grantee maximum window

---
+++ sql/grantee_windows.sql
@@ -0,0 +1,28 @@
+-- Grantee maximum window (>=3 in a year)
+
+WITH study_parcels AS (
+    SELECT das.*, mas.geometry
+    FROM rocket.detroit_assessors_sales das
+    JOIN rod.main_assessors mas
+        ON das.parcel_id = mas.parcel_id
+        AND das.sale_date::DATE > mas.start_date
+        AND das.sale_date::DATE <= mas.end_date
+    JOIN rocket.alpine_joy_study_areas ajsa
+        ON ST_WITHIN(st_transform(mas.geometry, 2898), ajsa.geometry)
+    WHERE ajsa.name = 'Original Boundaries'
+),
+transaction_window_counts AS (
+    SELECT grantee,
+           sale_date,
+           COUNT(*) OVER (
+                PARTITION BY grantee
+                ORDER BY sale_date::DATE
+                RANGE BETWEEN INTERVAL '1 year' PRECEDING AND CURRENT ROW
+           ) AS transaction_within_year
+    FROM study_parcels
+    ORDER BY 3 DESC
+)
+SELECT DISTINCT ON (grantee) grantee, sale_date, transaction_within_year
+FROM transaction_window_counts
+WHERE transaction_within_year >= 3
+ORDER BY grantee, transaction_within_year DESC, sale_date ASC;
