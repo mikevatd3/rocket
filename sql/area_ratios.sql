c1407c1700b03ae571dbea79e5ae002eef2c9e03
mikevatd3 <115018438+mikevatd3@users.noreply.github.com>
2026-06-30 08:50:13 -0400

    Refactor area ratio calculation for Detroit boundaries

--- sql/area_ratios.sql
+++ sql/area_ratios.sql
@@ -1,6 +1,18 @@
+WITH detroit_boundary AS (
+  SELECT *
+  FROM etl.geographies
+  WHERE geoid = '2616322000'
+  LIMIT 1  
+),
+detroit_area_intersections AS (
+  SELECT sas.name, 
+         ST_Intersection(ST_Transform(sas.geometry, 4269), detroit_boundary.geometry) AS geometry
+  FROM rocket.alpine_joy_study_areas sas
+  JOIN detroit_boundary ON TRUE
+)
 SELECT 
     name, 
-    ST_AREA(geometry) AS area, 
-    ST_AREA(geometry) / 4773002.517285653 AS ratio_to_orig
-FROM rocket.alpine_joy_study_areas sas
-ORDER BY 3;
+    ST_AREA(ST_TRANSFORM(geometry, 2898)) AS area, 
+    ST_AREA(ST_TRANSFORM(geometry, 2898)) / 4772820.613658167 AS ratio_to_orig
+FROM detroit_area_intersections
+ORDER BY 2;
